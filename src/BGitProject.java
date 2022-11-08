e8bb886b13259bcce9a525fadb4d644c1e66d9ee
Mehmet Maden <yasamtr@gmail.com>
2022-11-08 23:06:43 +0300

    Mr Ahmet Added a line

--- src/BGitProject.java
+++ src/BGitProject.java
@@ -7,5 +7,7 @@ public class BGitProject {
 
 
         System.out.println("ohh it is more than enough, thank you");
+
+        System.out.println("hey this belongs to mr. Ahmet. Do not change anything");
     }
 }
